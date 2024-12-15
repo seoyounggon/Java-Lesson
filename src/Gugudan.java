880facbe6e1369a83bd675dbc4cfdb381f1885f9
syg0628 <syg0628@naver.com>
2024-12-15 15:51:10 +0900

    2024년 12월 15일 추가

---
+++ src/Gugudan.java
@@ -0,0 +1,17 @@
+public class Gugudan {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int dan = 2;
+        while(dan<10) {
+            i = 1;
+            System.out.printf("%d단\n", dan);
+            while (i < 10) {
+                System.out.printf("%d X %d = %d\n",dan, i, dan * i);
+                i += 1;
+            }
+
+            dan+=1;
+        }
+    }
+}
